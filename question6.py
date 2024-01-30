7a4eb7aab2412e9a731f5b0edbe85f9bdeb28ab5
purnatummala <151711173+purnatummala@users.noreply.github.com>
2024-01-30 03:17:04 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,21 +1,21 @@
 def question6_1():
     answer = {
-        'equal_width': ['No change', 9],
-        'equal_frequency': ['No change', 9]
-    }
+    'equal_width': ['Change', 9],
+    'equal_frequency': ['Change', 9]
+}
     return answer
 
 def question6_2():
     answer = {
-        'equal_width': ['No change', 9],
-        'equal_frequency': ['No change', 9]
-    }
+    'equal_width': ['Change', 4],
+    'equal_frequency': ['Change', 4]
+}
     return answer
 
   
 def question6_3():
     answer = {
-        'equal_width': ['Change', 2],
-        'equal_frequency': ['Change', 2]
-    }
+    'equal_width': ['No change', 9],
+    'equal_frequency': ['No change', 9]
+}
     return answer
