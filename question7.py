0aa88bb4c1e9261236f672bd0855869ddfdb5dbc
purnatummala <151711173+purnatummala@users.noreply.github.com>
2024-01-30 03:38:49 -0500

    Update question7.py

--- question7.py
+++ question7.py
@@ -5,6 +5,7 @@ def question7_2():
     answer = 'increase/decrease'
     return answer
 def question7_3():
-    answer = [(0, 2), (3, 4), (5, 6), (7, 'infinity')]
+    answer = [(0.0, 2.0), (3.0, 4.0), (5.0, 6.0), (7.0, 'infinity')]
     return answer
+
 
